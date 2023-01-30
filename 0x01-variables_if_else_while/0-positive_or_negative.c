81ceae8922d6c6ceee877af36321c9d60af92191
erinscarter <erin.s.carter@gmail.com>
2023-01-29 20:16:36 -0600

    positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int num;
+
+	srand(time(0));
+	num = rand() - RAND_MAX / 2;
+
+	if (num > 0)
+		printf("%d is positive\n", num);
+	else if (num == 0)
+		primtf("%d is zero\n", num);
+	else 
+		printf("%d is negative\n", num);
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This directory contains tasks related to variables conditional statements and loops in C.
