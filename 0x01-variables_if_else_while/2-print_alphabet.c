cb28b2c633268ebaaad5435e85c9a8e9fc3a9108
erinscarter <erin.s.carter@gmail.com>
2023-01-30 20:58:57 -0600

    changed quotation marks

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
 *main - Entry point
 *
@@ -8,11 +6,10 @@
 */
 int main(void)
 {
-	char low;
+	char c;
 
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-		putchar('\n');
-
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
 	return (0);
 }
