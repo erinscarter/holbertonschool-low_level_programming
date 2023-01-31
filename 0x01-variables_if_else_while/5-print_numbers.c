a2bc62bca378b02992623044b02e08c1b08a2c43
erinscarter <erin.s.carter@gmail.com>
2023-01-30 22:04:56 -0600

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - prints numbers
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+	int base10;
+
+	for (base10 = 0; base10 < 10; base10++)
+	{
+		printf("%i", base10);
+	}
+	printf("\n");
+	return (0);
+}
