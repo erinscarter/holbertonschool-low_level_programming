6655cd9298a5295aea8564c5fd33359031fe16a0
erinscarter <erin.s.carter@gmail.com>
2023-02-01 10:51:00 -0600

    header

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include <main.h>
 /**
+* main - prints putchar
 *
-*
+* Return: 0
 *
 */
 
 int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+}
+	return (0);
 
