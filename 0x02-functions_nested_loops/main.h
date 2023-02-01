2b491f109cd4000bef4f188e4ae474883b857a18
erinscarter <erin.s.carter@gmail.com>
2023-02-01 10:56:39 -0600

    added include statements

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -12,4 +16,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-#endif
+
+#endif/*MAIN_H *?
