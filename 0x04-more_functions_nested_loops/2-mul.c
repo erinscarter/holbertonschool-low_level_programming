a5bdf587d5da8e46766aa02e36268f524b079eb2
erinscarter <erin.s.carter@gmail.com>
2023-02-05 14:55:57 -0600

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* mul - returns the product of two ints
+* @c: int given by the main function
+* @d: int given by the main function
+* Description: uses header to link and nest loops
+* Return: Returns two ints
+*/
+
+int mul(int c, int d)
+{
+	return (c * d);
+}
