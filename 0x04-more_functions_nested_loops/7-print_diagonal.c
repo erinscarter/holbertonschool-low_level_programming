697ff655abd240a7e32db600d9ad2c2449d111f4
erinscarter <erin.s.carter@gmail.com>
2023-02-05 17:23:57 -0600

    diagonal line, I will use a pencil

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_diagonal - prints diagonal line with a \n
+* @n: int given by main
+* Description: uses headers to link and a nested loop to execute
+* Return: void. No return.
+*/
+
+void diagonal_line(int n)
+{
+	int i, j;
+
+	if (n <= 8)
+		_putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+		for (j = n - i; j < n; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
