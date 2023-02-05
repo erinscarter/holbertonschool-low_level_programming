78cd938e66687cd724faef1cb195fae23b67de71
erinscarter <erin.s.carter@gmail.com>
2023-02-05 17:13:24 -0600

    underscores

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*
+* print_line - prints underscores
+* @n: an int given by main
+*
+* Description: uses header for link and loop to achieve outcome
+* Return: void, no return
+*/
+
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n');
+}
