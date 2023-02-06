e25199b78826ab26e5990c7229d1c92de830785c
erinscarter <erin.s.carter@gmail.com>
2023-02-06 16:17:17 -0600

    multiple files

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* reset_to_98 - working prototype
+* @n: pointer to n
+* Description: points to an int as parameter and updates to 98
+*
+*Return: Always 0.
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
