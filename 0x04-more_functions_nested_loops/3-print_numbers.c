efe716644f0c1b3022588d677d4829469f3a40f7
erinscarter <erin.s.carter@gmail.com>
2023-02-06 15:20:36 -0600

    typos again

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,6 +2,7 @@
 
 /**
 * print_numbers - prints 0 though 9 and new line
+*
 * Description: Uses headers to link and a nested loop to achieve outcome
 * Return: void, no return
 */
@@ -10,7 +11,7 @@ void print_numbers(void)
 {
 	int i = 0;
 
-	for (i >= 0; i <= 9; i++)
-	_putchar(i + '0');
+	for (i = 0; i < 10; i++)
+		_putchar(i + '0');
 	_putchar('\n');
 }
