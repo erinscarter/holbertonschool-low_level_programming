dc0f8f2f71194665bcac5f9b513019207c09983e
erinscarter <erin.s.carter@gmail.com>
2023-02-06 21:15:29 -0600

    print sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,12 +1,9 @@
 #include "main.h"
-#include "stdio.h"
 /**
 * print_sign - prints the sign of a mumber
-* @n: carrier variable
-* Description: prints the sign of a number
-* Return 1 if n > 0, 0 if n ==0, -1 if n < 0
+* @n: number input
+* Return: 1 prints '+' if n > 0, 0 prints '0' if n = 0, -1 prints '-' if n < 0
 */
-
 int print_sign(int n)
 {
 	if (n > 0)
@@ -14,11 +11,14 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n < 0)
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
 	{
 		_putchar('-');
 		return (-1);
 	}
-	_putchar('0');
-	return (0);
 }
