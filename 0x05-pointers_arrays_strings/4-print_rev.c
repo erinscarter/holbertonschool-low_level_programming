9cb449c4f5b425918fbe46c3c619e5ebc7fe5c81
erinscarter <erin.s.carter@gmail.com>
2023-02-06 21:02:03 -0600

    code overhaul

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,18 +2,26 @@
 
 /**
 * print_rev - prints a string in reverse
-*
+* fcounter is to first count to end, n is to count back
 * @s: int to check
 *
 * Description: this will print a string in reverse
-* Return: 0 is success
+* Return: string in reverse
 */
 
 void print_rev(char *s)
 {
-	int string = 0;
+	int fcounter = 0;
+	int i, n;
 
-	for (string = 0; string >= 0; string--)
-		_putchar(s[string]);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		fcounter++;
+	}
+
+	for (n = (fcounter - 1); n >= 0; n--)
+	{
+		_putchar(s[n]);
+	}
 	_putchar('\n');
 }
