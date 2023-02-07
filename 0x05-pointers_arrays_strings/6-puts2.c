e26eb56818fb0dd70bc30e0aa103e0692b57efe3
erinscarter <erin.s.carter@gmail.com>
2023-02-06 20:14:03 -0600

    every other one

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* puts2 - prints every other char of a string
+*
+* @str: char to check
+*
+* Return: 0 is success
+*/
+
+void puts2(char *str)
+{
+	int string;
+
+	for (string = 0; str[string] != '\0'; string++)
+	{
+	if (string % 2 == 0)
+		_putchar(str[string]);
+	else
+		continue;
+	}
+		_putchar('\n');
+}
