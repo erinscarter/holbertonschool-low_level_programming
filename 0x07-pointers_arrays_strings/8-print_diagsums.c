f6e95d5716bd9b64880fad90d31a73b1aede1915
erinscarter <erin.s.carter@gmail.com>
2023-02-13 19:22:34 -0600

    pointers, strings and arrays

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_diagsums - prints the sum of two diagonals of a square matrix
+* @a: the matrix
+* @size: the length of the arrays
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sum1, sum2;
+	int len;
+
+	i = 0;
+	sum1 = sum2 = 0;
+	len = size * size;
+	while (i < len)
+	{
+		if (i % (size - 1) == 0 && i < len -1 && i > 0)
+			sum2 += *(a + i);
+		if (i % (size + 1) == 0 || i == 0)
+			sum1 += *(a + i);
+		i++;
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,8 @@
+#include "holberton.h"
+
+/**
+* _puts_recursion - function like puts();
+* @s: input
+* Return: Always 0 (Success)
+*/
+
