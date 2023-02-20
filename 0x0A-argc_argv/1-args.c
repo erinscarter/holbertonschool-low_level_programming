b776fe815624f2d07a180a0a5a36a6341897b65b
erinscarter <erin.s.carter@gmail.com>
2023-02-20 16:51:53 -0600

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - print its name
+* @argc: argument count
+* @argv: argument vector for values
+*
+* Return: Always 0 (Success)
+*/
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (argc--)
+	{
+		i++;
+	}
+	i--;
+	if (argv[0])
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
