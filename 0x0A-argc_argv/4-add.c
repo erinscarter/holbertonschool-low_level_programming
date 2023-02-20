1f770b680aa9dde788df1dc7d0bc16b3e9f9489d
erinscarter <erin.s.carter@gmail.com>
2023-02-20 17:20:25 -0600

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+* main - entry point
+* @argc: argument count
+* @argv: argument vector for values
+*
+* Return: Always 0 (Success)
+*/
+
+int main(int argc, char **argv)
+{
+	int num, result = 0, i;
+
+	while (argc-- > 1)
+	{
+		for (i = 0; argv[argc][i]; i++)
+		{
+			if (!(isdigit(argv[argc][i])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		num = atoi(argv[argc]);
+		result += num;
+	}
+	printf("%d\n", result);
+	return (0);
+}
