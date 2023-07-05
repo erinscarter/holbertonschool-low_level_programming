9c2cd7d08c265cf8ab024646e7c89f2f53a676f5
erinscarter <erin.s.carter@gmail.com>
2023-07-05 16:45:42 -0500

    initial setup

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,40 +1,31 @@
-#include "variadic.h"
-/*
- * *
- * *
- * **/
-
+#include "variadic_functions.h"
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
-
+/**
+* sum_them_all - return the sum of all parameters.
+*
+* @n: variadic ints in list.
+*
+* Return: int - sum of all variadic list inputs
+*/
 int sum_them_all(const unsigned int n, ...)
 {
-    if (n == 0)
-        return 0;
+	unsigned int index, sum = 0;
+	va_list args;
 
-    int sum = 0;
-    va_list args;
-    va_start(args, n);
+	if (n == 0)
+	{
+		return (0);
+	}
+	va_start(args, n);
 
-    for (unsigned int i = 0; i < n; ++i) {
-        int num = va_arg(args, int);
-        sum += num;
-    }
-
-    va_end(args);
-    return sum;
+	for (index = 0; index < n; index++)
+	{
+		sum += va_arg(args, const unsigned int);
+	}
+	va_end(args);
+	return (sum);
 }
-
-int main()
-{
-    int sum1 = sum_them_all(3, 1, 2, 3);
-    int sum2 = sum_them_all(5, 10, 20, 30, 40, 50);
-    int sum3 = sum_them_all(0);
-
-    printf("Sum1: %d\n", sum1);
-    printf("Sum2: %d\n", sum2);
-    printf("Sum3: %d\n", sum3);
-
-    return 0;
 }
 
